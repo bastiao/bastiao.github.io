038f19280c9357367b73b4a9e8d3cc166b0033df
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-11-30 14:13:57 +0000

    Added UI routes

--- js/bastiao.core.js
+++ js/bastiao.core.js
@@ -1,10 +1,17 @@
+
 function Ctrl($scope) {
   $scope.templates =
-    [ { name: 'Home', url: 'h.omehtml'},
-      { name: 'Projects', url: 'projects.html'},
-      { name: 'Publications', url: 'publications.html'},
-      { name: 'Blog', url: 'blog.html'},
-      { name: 'Contact', url: 'contact.html'},
-      { name: 'About', url: 'about.html'} ];
+    [ { name: 'Home', url: 'templates/home.html'},
+      { name: 'Projects', url: 'templates/projects.html'},
+      { name: 'Publications', url: 'templates/publications.html'},
+      { name: 'Blog', url: 'templates/blog.html'},
+      { name: 'Contact', url: 'templates/contact.html'},
+      { name: 'About', url: 'templates/about.html'} ];
   $scope.template = $scope.templates[0];
+  $scope.loadTemplate = function(tmp){
+    alert(tmp);
+  };
 };
+
+
+
