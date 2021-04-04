a8a44877a473892024602b633fc7bf6dcfb6bd04
Luís Bastião Silva <bastiao@bmd-software.com>
2021-04-04 17:52:11 +0100

    Avoid cache on my website

--- js/states.js
+++ js/states.js
@@ -1,8 +1,13 @@
 // Make sure to include the `ui.router` module as a dependency.
+
+// Update here, to make sure, it will avoid pages to be cached.
+var version = "1.0.2";
+
+
 angular.module('uiRouterSample')
   .config(
-    [          '$stateProvider', '$urlRouterProvider',
-      function ($stateProvider,   $urlRouterProvider) {
+    ['$stateProvider', '$urlRouterProvider',
+      function ($stateProvider, $urlRouterProvider) {
 
         /////////////////////////////
         // Redirects and Otherwise //
@@ -11,12 +16,9 @@ angular.module('uiRouterSample')
         // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
         $urlRouterProvider
 
-          // The `when` method says if the url is ever the 1st param, then redirect to the 2nd param
-          // Here we are just setting up some convenience urls.
           .when('/c?id', '/contacts/:id')
           .when('/user/:id', '/contacts/:id')
 
-          // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
           .otherwise('/');
 
 
@@ -33,13 +35,8 @@ angular.module('uiRouterSample')
 
           .state('home', {
 
-            // This abstract state will prepend '/contacts' onto the urls of all its children.
             url: '/',
-
-            // Example of loading a template from a file. This is also a top level state,
-            // so this template file will be loaded and then inserted into the ui-view
-            // within index.html.
-            templateUrl: 'templates/home.html',
+            templateUrl: 'templates/home.html?v=' + version,
 
           })
 
@@ -51,14 +48,8 @@ angular.module('uiRouterSample')
 
           .state('blog', {
 
-            // This abstract state will prepend '/contacts' onto the urls of all its children.
             url: '/blog',
-
-            // Example of loading a template from a file. This is also a top level state,
-            // so this template file will be loaded and then inserted into the ui-view
-            // within index.html.
-            templateUrl: 'templates/blog.html',
-
+            templateUrl: 'templates/blog.html?v=' + version,
           })
 
 
@@ -69,13 +60,8 @@ angular.module('uiRouterSample')
 
           .state('contact', {
 
-            // This abstract state will prepend '/contacts' onto the urls of all its children.
             url: '/contact',
-
-            // Example of loading a template from a file. This is also a top level state,
-            // so this template file will be loaded and then inserted into the ui-view
-            // within index.html.
-            templateUrl: 'templates/contact.html',
+            templateUrl: 'templates/contact.html?v=' + version,
 
           })
 
@@ -87,14 +73,8 @@ angular.module('uiRouterSample')
           //////////////
 
           .state('projects', {
-
-            // This abstract state will prepend '/contacts' onto the urls of all its children.
             url: '/projects',
-
-            // Example of loading a template from a file. This is also a top level state,
-            // so this template file will be loaded and then inserted into the ui-view
-            // within index.html.
-            templateUrl: 'templates/projects.html',
+            templateUrl: 'templates/projects.html?v=' + version,
 
           })
 
@@ -107,37 +87,17 @@ angular.module('uiRouterSample')
 
           .state('publications', {
 
-            // This abstract state will prepend '/contacts' onto the urls of all its children.
             url: '/publications',
-
-            // Example of loading a template from a file. This is also a top level state,
-            // so this template file will be loaded and then inserted into the ui-view
-            // within index.html.
-            templateUrl: 'templates/publications.html',
+            templateUrl: 'templates/publications.html?v=' + version,
 
           })
-
-
-
-
 
           //////////////
           // About //
           //////////////
 
           .state('about', {
-
-            // This abstract state will prepend '/contacts' onto the urls of all its children.
             url: '/about',
-
-            // Example of loading a template from a file. This is also a top level state,
-            // so this template file will be loaded and then inserted into the ui-view
-            // within index.html.
-            templateUrl: 'templates/about.html',
-
+            templateUrl: 'templates/about.html?v=' + version,
           })
-
-
-
-
       }]);
