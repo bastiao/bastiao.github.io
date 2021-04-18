d5d1d3ff0a3afad5dbabd87c03d4a45ba966be40
Luís Bastião Silva <bastiao@bmd-software.com>
2021-04-18 12:50:58 +0100

    adjustments in mobile version

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,47 +1,51 @@
 (function($) {
-  "use strict"; // Start of use strict
+    "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 56)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
-  });
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 56
+    });
 
-  var typed = new Typed('.typehashtags', {
-    strings: ['archicture', 'software', 'cloud native', 'DICOM', 'PACS', 'docker', 'kubernetes', 'scalability', 'management', 'leadership', 'programming' ],
-    smartBackspace: true, // Default value,
-    loop: true,
-    backSpeed: 70,
-    typeSpeed: 70,
-    backDelay: 400,
-    startDelay: 0
-  });
+    var typed = new Typed('.typehashtags', {
+        strings: ['archicture', 'software', 'cloud native', 'DICOM',
+            'PACS', 'docker', 'innovation', 'kubernetes', 'scalability', 'think different',
+            'resiliency',
+            'management', 'programming'
+        ],
+        smartBackspace: true, // Default value,
+        loop: true,
+        backSpeed: 70,
+        typeSpeed: 70,
+        backDelay: 400,
+        startDelay: 0
+    });
 
-  
+
 
 })(jQuery); // End of use strict
 
 
 // Arrow on home page
-$(window).scroll(function(){
-  $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);  //250 is fade pixels
+$(window).scroll(function() {
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); //250 is fade pixels
 });
